73f33cfc84de60df8bb0090287e9adea88a67e78
Dwire <gjdwyer8@gmail.com>
2018-08-13 17:35:19 -0400

    refactor-one done....for now

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
   getChores()
-  const choreList = document.getElementById('chore-list')
-  const newChoreFormSubmit = document.getElementById('submit-btn')
-  choreList.addEventListener('click', deleteChore)
-  newChoreFormSubmit.addEventListener('click', newChoreInputs)
-
+  //--------------------------- Fetch Actions (REST/CRUD) ------------------------------
   function getChores(){
     fetch('http://localhost:3000/chores')
     .then(res => res.json())
-    .then(displayChores)
+    .then(choreObject)
   }
 
+  function postChore(choreObj){
+    fetch('http://localhost:3000/chores', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(choreObj)
+    })
+    .then(res => res.json())
+    .then(choreObject)
+  }
 
-  function newChoreInputs(e){
+  function deleteChore(choreID){
+    fetch(`http://localhost:3000/chores/${choreID}`, {
+      method:"DELETE"})
+    }
+
+
+// ---------------------Grab Container divs from html ----------------
+  const choreList = document.getElementById('chore-list')
+  const newChoreForm = document.getElementById('new-chore-form')
+  choreList.addEventListener('click', targetChoreToDelete)
+  newChoreForm.addEventListener('submit', newChoreInputs)
+
+// ----------------------Function to Manipulate Dom Elements---------------
+
+  function newChoreInputs(e){ //Gets chore object from form input fields
     e.preventDefault()
     const titleInput = document.getElementById('title').value
     const priorityInput = document.getElementById('priority').value
@@ -24,42 +45,39 @@ document.addEventListener('DOMContentLoaded', () => {
       duration: durationInput
     }
 
-    fetch('http://localhost:3000/chores', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(choreObject)
-    })
-    .then(res => res.json())
-    .then(console.log)
-
-    // console.log(choreObject);
-
-    // console.log(titleInput, priorityInput, durationInput);
+    postChore(choreObject)
+    newChoreForm.reset()
   }
 
-
-  function displayChores(res){
-    res.forEach(chore => {
-      const choreDiv = document.createElement('div')
-      const deleteButton = document.createElement('button')
-
-      choreDiv.innerHTML = `
-        <h3> Title: ${chore.title} </h3>
-        <p>${chore.priority}</p>
-        <p>${chore.duration}</p>
-        <button class='delete-button' data-id=${chore.id}>X</button>
-      `
-
-      choreDiv.className = 'chore-card'
-      choreList.appendChild(choreDiv)
-
-    })
+  function choreObject(res){ //Takes responce from both GET and POST methods and gives correct format to dispalyChores()
+    if (Array.isArray(res)) {
+      res.forEach(chore => displayChores(chore))
+    }else{
+      displayChores(res)
+    }
   }
 
-  function deleteChore(e){
-    // console.log(e.target);
+  function displayChores(chore){ //takes a chore object and displays it to the dom
+    const choreDiv = document.createElement('div')
+    const deleteButton = document.createElement('button')
+
+    choreDiv.innerHTML = `
+      <h3> Title: ${chore.title} </h3>
+      <p>${chore.priority}</p>
+      <p>${chore.duration}</p>
+      <button class='delete-button' data-id=${chore.id}>X</button>
+    `
+    choreDiv.className = 'chore-card'
+    choreList.appendChild(choreDiv)
+
+  }
+
+  function targetChoreToDelete(e){
+    if (event.target.className == "delete-button"){
+      event.target.parentElement.remove()
+      let choreID = event.target.getAttribute('data-id')
+      deleteChore(choreID)
+    }
   }
 
 })
